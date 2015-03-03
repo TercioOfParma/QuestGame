92aed94b242d4290cae55200114b9fdc38f42dcd
kburk1997 <kburk97@optonline.net>
2015-03-03 18:51:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,32 +8,35 @@ int main(int argc, char* argv[])
 {
 	init(argv); //address to an address so we can access the entire array
 	string input; //given string because this is what works. if you have another way be sure to show!
-	bool flag = false;
-	
-
-    
-    cout<<"You wake up and go outside, do you go left or right?\n";
-    
-    //puts this in a loop incase invalid input was given, could be made into a function surely. 
-	do{
-    cout<<"\n1. Left\n"; //we want more than just left and right surely, so this should be different
-    cout<<"\n2. Right\n";
-    
-	cin>>input;
-    if(input == "1"|| input == "2") 
-    	flag = true;
-    else 
-		cout<<"The hell you doing? Input numbers only!";
-    
-    
-}while(flag == false);
-
-if(input == "1")
-	 cout<<"Continuing story #1. . . .";
- else if (input == "2" )
-	 cout<<"Continuing story #2. . . .";
+	beginQuest(input); //call beginQuest with input
 	 
 	 return 0;
 }
 
+int beginQuest(String input){ //Putting do-while into function
+	bool flag = false;
+	cout<<"You wake up and go outside, do you go left or right?\n";
+
+	 //puts this in a loop incase invalid input was given, could be made into a function surely. 
+	do{
+	    	cout<<"\n1. Left\n"; //we want more than just left and right surely, so this should be different
+	    	cout<<"\n2. Right\n";
+    
+		cin>>input;
+	
+		if(input == "1"|| input == "2") 
+		    	flag = true;
+		else 
+			cout<<"The hell you doing? Input numbers only!";
+		    
+	}while(flag == false);
+
+	if(input == "1")
+		 cout<<"Continuing story #1. . . .";
+	 else if (input == "2" )
+		 cout<<"Continuing story #2. . . .";
+		 
+	return 0;
+
+}
 
